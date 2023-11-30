4f50dfd2d0c503a65c48947c3878cea869e53fd4
Ferfo1 <ferfo1hivatalos@gmail.com>
2023-11-30 17:16:46 +0100

    make game over

--- Code/code/code.ino
+++ Code/code/code.ino
@@ -7,7 +7,7 @@ const int loopPin = 7;          // Digital pin for the loop wire
 const int buzzerPin = 8;        // Digital pin for the buzzer
 const int startButtonPin = 6;   // Digital pin for the start button
 const int overButtonPin = 9;
-const unsigned long gameTime = 12000;  // 120 seconds (2 minutes) in milliseconds
+const unsigned long gameTime = 120000;  // 120 seconds (2 minutes) in milliseconds
 const int rledPin = 2;
 const int gledPin = 3;
 
@@ -21,13 +21,14 @@ void setup() {
   pinMode(loopPin, INPUT_PULLUP);       // Set loopPin as input with pull-up resistor
   pinMode(buzzerPin, OUTPUT);
   pinMode(startButtonPin, INPUT_PULLUP); // Set startButtonPin as input with pull-up resistor
+  pinMode(overButtonPin, INPUT_PULLUP);
   pinMode(rledPin, OUTPUT);
   pinMode(gledPin, OUTPUT);
   
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Wire Loop Game");
+  lcd.print("Vonal Vezeto");
   delay(2000);
   lcd.clear();
 }
@@ -36,14 +37,14 @@ void loop() {
   if (!gameRunning) {
     lcd.setCursor(0, 0);
     
-    lcd.print("Raise the wand");
+    lcd.print("Emeld fel a palc");
     lcd.setCursor(0, 1);
-    lcd.print("to start!");
+    lcd.print("at a kezdehez!");
 
     if (digitalRead(startButtonPin) == HIGH) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Game started!");
+      lcd.print("Jatek inditva!");
       digitalWrite(gledPin, HIGH);
       delay(1000);
       digitalWrite(gledPin, LOW);
@@ -57,28 +58,34 @@ void loop() {
     if (elapsedTime >= gameTime) {
       endGame();
     }
+    if (digitalRead(overButtonPin) == LOW){
+      endGame();
+    }
+    
 
     lcd.setCursor(0, 0);
-    lcd.print("Time: ");
+    lcd.print("Ido: ");
     lcd.print((gameTime - elapsedTime) / 1000);  // Display remaining time in seconds
     lcd.print("s ");
     
     lcd.setCursor(0, 1);
-    lcd.print("Wire the loop! ");
-    lcd.print("Count: ");
+    lcd.print("Vezesd a hurkot! ");
+    lcd.print("Szamlalo: ");
     lcd.print(loopCount);
 
     if (digitalRead(loopPin) == LOW) {
       loopCount++;
       lcd.clear();
       digitalWrite(rledPin, HIGH);
+      digitalWrite(buzzerPin, HIGH);
       lcd.setCursor(0, 0);
-      lcd.print("Good job!");
+      lcd.print("Hozzaertel!");
       lcd.setCursor(0, 1);
-      lcd.print("Count: ");
+      lcd.print("Szamlalo: ");
       lcd.print(loopCount);
       delay(1000);
       digitalWrite(rledPin, LOW);
+      digitalWrite(buzzerPin, LOW);
       lcd.clear();
     }
   }
@@ -94,23 +101,24 @@ void endGame() {
   gameRunning = false;
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Game over!");
+  lcd.print("Jatek vege!");
   lcd.setCursor(0, 1);
-  lcd.print("Count: ");
+  lcd.print("Szamlalo: ");
   lcd.print(loopCount);
   digitalWrite(buzzerPin, HIGH);  // Turn on the buzzer
+  digitalWrite(rledPin, HIGH);
   delay(1000);
   digitalWrite(buzzerPin, LOW);   // Turn off the buzzer
+  digitalWrite(rledPin, LOW);
+  delay(5000);
   lcd.clear();
-  delay(2000);
-  lcd.print("Return the stick to");
+  lcd.print("Tedd vissza a ");
   lcd.setCursor(0, 1);
-  lcd.print("the start!");
+  lcd.print("kezdeshez!");
 
   // Wait until the start button is pressed to clear the display
   while (digitalRead(startButtonPin) == HIGH) {
     delay(100);
-    
-  }
+    }
   lcd.clear();
 }
